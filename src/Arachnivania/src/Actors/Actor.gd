9ff93c6a1b8f9d14de46833db503808141804844
Ryan Goode <33633342+rngoode1@users.noreply.github.com>
2021-02-26 13:56:34 -0500

    integrated my code and adjusted phyics to match yous. added jump animation code, but commented out until its ready. Refs #40

--- src/Arachnivania/src/Actors/Actor.gd
+++ src/Arachnivania/src/Actors/Actor.gd
@@ -5,6 +5,12 @@ const FLOOR_NORMAL: = Vector2.UP
 
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
+#const ACCELERATION = 512
+#const MAX_SPEED = 100
+#const FRICTION = 0.30
+#const AIR_RESISTANCE = 0.05
+#const GRAVITY = 200
+#const JUMPFORCE = 140
 
 var _velocity: = Vector2.ZERO
 # Use onready var to grab children nodes, like the sprite
--- src/prototype/src/Actors/Player.gd
+++ src/prototype/src/Actors/Player.gd
@@ -27,3 +27,35 @@ func calculate_move_velocity(
 		if is_jump_interrupted:
 			out.y = 0.0 
 		return out
+
+#const ACCELERATION = 512
+#const MAX_SPEED = 100
+#const FRICTION = 0.30
+#const AIR_RESISTANCE = 0.05
+#const GRAVITY = 200
+#const JUMPFORCE = 140
+#var motion = Vector2.ZERO
+
+## This handles the left and right movement
+#func _physics_process(delta):
+#	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+#	if x_input != 0:
+#		animationPlayer.play("Walk")
+#		motion.x += x_input * ACCELERATION * delta
+#		motion.x = clamp(motion.x, -MAX_SPEED,MAX_SPEED)
+#		sprite.flip_h = x_input < 0
+#	else:
+#		animationPlayer.play("Stand")
+#	motion.y += GRAVITY * delta
+#	if is_on_floor():
+#		if x_input == 0:
+#			motion.x = lerp(motion.x, 0, FRICTION)
+#		if Input.is_action_just_pressed("ui_up"):
+#			motion.y = -JUMPFORCE
+#	else:
+#		animationPlayer.play("jump")
+#		if Input.is_action_just_released("ui_up") and motion.y < -JUMPFORCE/2:
+#			motion.y = -JUMPFORCE/2 
+#		if x_input == 0: 
+#			motion.x = lerp(motion.x, 0, AIR_RESISTANCE)
+#	motion = move_and_slide(motion,Vector2.UP)

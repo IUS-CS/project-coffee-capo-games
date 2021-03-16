f74dc3cd284d46f4291549a60a53ca9f7108e352
Ryan Goode <33633342+rngoode1@users.noreply.github.com>
2021-03-16 19:37:59 -0400

    Experience and Health
    * Added autoloads (singleton; accessed globally) to track player data
    * Health starts at 100 and Xp starts at 0
    * Enemy kills grant 100 xp
    * Enemy damage inflicts 25 health
    * HealthOrbs grant 20 health
    * Max health is 100, if health < 0, you die
    
    Refs #76 #75 #43

--- src/Arachnivania/src/Objects/HealthOrb.gd
+++ src/Arachnivania/src/Objects/HealthOrb.gd
@@ -2,5 +2,14 @@ extends Area2D
 
 onready var animationPlayer: AnimationPlayer = get_node("AnimationPlayer")
 
+export var health: = 20
+
 func _on_body_entered(_body: PhysicsBody2D) -> void:
+	if PlayerData.health <= 100:
+		picked()
+
+func picked() -> void:
+	PlayerData.health += health
 	animationPlayer.play("fade_out")
+	if PlayerData.health > 100:
+		PlayerData.health = 100

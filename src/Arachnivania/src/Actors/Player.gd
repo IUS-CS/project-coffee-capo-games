21d4a4d8c0df5019c41fde52f8e4de60066769fa
Ryan Goode <33633342+rngoode1@users.noreply.github.com>
2021-03-25 16:06:19 -0400

    Reorganized and Added Icon Assets
    * Added Grassy Plains asset added by Ryan S to src/assets folder
    * Reorganized src/assets folder for better clarity
    * Added state machine icons to src/assets imported to project
    
    Refs #83 #45

--- src/Arachnivania/src/Actors/Player.gd
+++ src/Arachnivania/src/Actors/Player.gd
@@ -28,7 +28,7 @@ func _physics_process(_delta: float) -> void:
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
 
 func calculate_move_velocity(
@@ -43,7 +43,7 @@ func calculate_move_velocity(
 		if direction.y ==  -1.0:
 			out.y = speed.y * direction.y
 		if is_jump_interrupted:
-			out.y = 0.0 
+			out.y = 1.0 
 		return out
 
 func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:

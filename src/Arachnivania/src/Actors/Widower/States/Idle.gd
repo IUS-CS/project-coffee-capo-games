e90c6928872a854a17c66b5749a9b74eadd071fd
Ryan Goode <rngoode1@hotmail.com>
2021-03-29 15:06:45 -0400

    Idle State
    * Coded the Idle.gd state
    	* added default state interface functions
    	* unhandled input delgates to Move parent
    	* physics process handles transition to either Run or Air
    	* when entered, parent Moves state gets entered and max_speed
    	  and velocity are reset to default
    
    Refs #84 #95

--- src/Arachnivania/src/Actors/Widower/States/Idle.gd
+++ src/Arachnivania/src/Actors/Widower/States/Idle.gd
@@ -1,2 +1,26 @@
 extends State
 
+#Base interface function from state, delegates to Move
+func unhandled_input(event: InputEvent) -> void:
+	var move: = get_parent()
+	move.unhandled_input(event)
+
+#Base interface function from state, defines Idle and transitions
+func physics_process(delta: float) -> void:
+	var move: = get_parent()
+	if owner.is_on_floor() and move.get_move_direction().x != 0.0:
+		_state_machine.transition_to("Move/Run")
+	elif not owner.is_on_floor():
+		_state_machine.transition_to("Move/Air")
+	
+
+#Base interface function from state to delegate to the parent state Move
+func enter(msg: Dictionary = {}) -> void:
+	var move: = get_parent()
+	move.enter(msg)
+	move.max_speed = move.max_speed_default
+	move.velocity = Vector2.ZERO
+
+#Base interface function from state to delegate to the parent state Move
+func exit() -> void:
+	return

430ad7ddd31b0aec94780114c1187df476b304ed
Ryan Goode <rngoode1@hotmail.com>
2021-03-29 16:23:43 -0400

    Run State
    * Added code to the Run.gd state
    	* Delegated unhandled_input, enter and exit to parent Move state
    	* Transition to Idle or Air depending on move direction and if
    	  the player is on the floor
    * Update Idle.gd exit() function to delegate to parent Move state
    
    Refs #84 #95

--- src/Arachnivania/src/Actors/Widower/States/Run.gd
+++ src/Arachnivania/src/Actors/Widower/States/Run.gd
@@ -8,12 +8,18 @@ func unhandled_input(event: InputEvent) -> void:
 #Base interface function from state, defines Run and transitions
 func physics_process(delta: float) -> void:
 	var move: = get_parent()
+<<<<<<< HEAD
 	#Transitions to other states based on direction and position
+=======
+>>>>>>> 3aa1ee9... Run State
 	if owner.is_on_floor() and move.get_move_direction().x == 0.0:
 		_state_machine.transition_to("Move/Idle")
 	elif not owner.is_on_floor():
 		_state_machine.transition_to("Move/Air")
+<<<<<<< HEAD
 	#delegates physics process to move
+=======
+>>>>>>> 3aa1ee9... Run State
 	move.physics_process(delta)
 
 #Base interface function from state to delegate to the parent state Move

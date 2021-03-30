a7389cc9e4dee796118365593136a127e01bcdd1
Ryan Goode <33633342+rngoode1@users.noreply.github.com>
2021-03-30 19:28:10 -0400

    Added Air state and tweaked movement
    * Added Air.gd state script
    	* Added transition conditions based on position of player after
    	  movement
    	* delegate movement to move and checks for msg on enter that
    	  will adjust velocity based on keyword
    	* Added a calculate jump velocity function that just returns the
    	  move.calculate_velocity, taking moves velocity and max_speed,
    	  acceleration of the impulse property in the y.UP direction
    * Added unhandled_input function to Move in order to handle the jump
      input and transition to Air state. This way it covers both Idle and
      Run
    * Tweaked the default physics variables to feel more like the original
      test scene. Reduced the jump impulse to rely more on the upcoming hook
      ability
    * Updated some comments in various scripts
    
    Refs #84 #95 #106

--- src/Arachnivania/src/Actors/Widower/States/Run.gd
+++ src/Arachnivania/src/Actors/Widower/States/Run.gd
@@ -15,6 +15,7 @@ func physics_process(delta: float) -> void:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	#Transitions to other states based on direction and position
 =======
 >>>>>>> 3aa1ee9... Run State
@@ -23,14 +24,21 @@ func physics_process(delta: float) -> void:
 >>>>>>> f351235... Added Air state and tweaked movement
 =======
 >>>>>>> 3aa1ee9... Run State
+=======
+	#Transitions to other states based on direction and position
+>>>>>>> f351235... Added Air state and tweaked movement
 	if owner.is_on_floor() and move.get_move_direction().x == 0.0:
 		_state_machine.transition_to("Move/Idle")
 	elif not owner.is_on_floor():
 		_state_machine.transition_to("Move/Air")
 <<<<<<< HEAD
+<<<<<<< HEAD
 	#delegates physics process to move
 =======
 >>>>>>> 3aa1ee9... Run State
+=======
+	#delegates physics process to move
+>>>>>>> f351235... Added Air state and tweaked movement
 	move.physics_process(delta)
 
 #Base interface function from state to delegate to the parent state Move

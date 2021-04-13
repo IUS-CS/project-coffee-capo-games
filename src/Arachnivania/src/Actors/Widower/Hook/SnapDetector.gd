18803e5a6fd377c674e39edc6c6c40f2c05791a3
Ryan Goode <33633342+rngoode1@users.noreply.github.com>
2021-04-13 17:28:14 -0400

    Snap Detector
    * Added `CollisionShape2D` with a `CapsuleShape2D` to fit over the hook head, named `CapsuleV`
    * Added `RayCast2D` to detect obstacles
    * Added `Position2D` named `HookingHint` that will draw a indicator over the `HookingTarget`
    * Attached script `HookingHint.gd` to `HookingHint` node
        * tool, to show in editor
        * Extends `Node2D`
        * #Properties
        * #Draw function
        * #Making the node completly independant, can place and draw anywhere from parent making the hooking hint constant regardless of snap detector position
    * Coded `SnapDetector.gd`
        * #References to Node dependencies
        * #Store HookTarget setter
        * #Set_target setter function, only enables hooking_hint if there is a HookTarget
        * #Determine if there is a target or not
        * #Set_target setter function, only enables hooking_hint if there is a HookTarget

--- src/Arachnivania/src/Actors/Widower/Hook/SnapDetector.gd
+++ src/Arachnivania/src/Actors/Widower/Hook/SnapDetector.gd
@@ -1,2 +1,23 @@
 extends Area2D
+"""
+A seperate object from the hook that will find the hooking target
+"""
+
+#References to Node dependencies
+onready var hooking_hint: Position2D = $HookingHint
+onready var ray_cast: RayCast2D = $RayCast2D
+
+#Store HookTarget setter
+var target: HookTarget setget set_target
+
+#Determine if there is a target or not
+func has_target() -> bool:
+	return target != null
+
+#Set_target setter function, only enables hooking_hint if there is a HookTarget
+func set_target(value: HookTarget) -> void:
+	target = value
+	hooking_hint.visible = has_target()
+	hooking_hint.global_position = target.global_position if target else \
+	hooking_hint.global_position
 

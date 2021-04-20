036e8744fb2bc7bdd9fd5affafe24cfd47b1aec7
Nathan Tate <nathantate14@gmail.com>
2021-04-20 18:10:48 -0400

    moved old UI files to new folder in src/main

---
+++ src/Arachnivania/srcPreState/UserInterfaceOLD/MainMenuButton.gd
@@ -0,0 +1,13 @@
+tool
+extends Button
+
+# creates inspector option with a file path as input
+export(String, FILE) var next_scene_path: = "Title.tscn"
+
+func _on_button_up():
+	PlayerData.reset()
+	get_tree().paused = false
+	get_tree().change_scene(next_scene_path)
+
+func _get_configuration_warning() -> String:
+	return "next_scene_path must be set for button to work" if next_scene_path == "" else ""
